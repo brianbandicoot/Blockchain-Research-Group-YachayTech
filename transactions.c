0bb0da19c1a5dd19ac7b44d141e9e6f368a0b702
NicoSerranoP <38594836+NicoSerranoP@users.noreply.github.com>
2019-05-01 23:36:01 -0500

    Blockchain watcher is done!
    We are having some issues when a block is modified in our data file.

---
+++ transactions.c
@@ -0,0 +1,24 @@
+/* This file is not ready yet but we need to implement
+this for the Information Security class.
+
+1. The merkleRoot is a binary tree built with the SHA256 hashes
+of all the transactions which will be stored in the block
+
+2. The digitalSignature will use the RSA Signature algorithm
+to validate who owns the transaction
+
+3. The getPublicKey will be used by each user to get the publickey
+from their privatekey
+
+*/
+char* getMerkleRoot(char TransactionPool[]){
+
+}
+
+char* digitalSignature(char Transaction[]){
+
+}
+
+int getPublicKey(){
+
+}
